dd99231576608927344fc5141f82f1e0671cb083
Juan Ignacio Scapellato <juanignacio295@hotmail.com>
2024-05-22 20:16:04 -0300

    rr

--- Tecno_Coder/vite.config.js
+++ Tecno_Coder/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/PreEntrega2-scapellato-/', 
   plugins: [
     react({
       fastRefresh: {
@@ -10,4 +11,3 @@ export default defineConfig({
     }),
   ],
 });
-
